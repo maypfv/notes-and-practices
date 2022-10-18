0fdf9c5beb9d7c294b93d4e9117b02cfdaecbc23
Source Academy User <No public email provided>
2022-10-18 11:13:44 +0800

    Changes made from Source Academy

--- reflection9-sorting.js
+++ reflection9-sorting.js
@@ -1,25 +1,62 @@
-function copy(A) {
-    const new_A = A;
-    return new_a;
-}
-function find_min_pos(A, low, hi) {
-    let min_pos = low;
-    for (let j = low + 1; j <= high; j = j + 1) {
-        if (A[j] < A[min_pos]) {
-            min_pos = j;
-        }
+// function copy(A) {
+//     const new_A = A;
+//     return new_a;
+// }
+// function find_min_pos(A, low, hi) {
+//     let min_pos = low;
+//     for (let j = low + 1; j <= high; j = j + 1) {
+//         if (A[j] < A[min_pos]) {
+//             min_pos = j;
+//         }
+//     }
+// }
+// function selection_sort(A) {
+//     const len = array_length(A);
+//     for (let i = 0; i < len; i = i + 1) {
+//         let min_pos = find_min_pos(A, i, len - 1);
+//         swap(A, i, min_pos);
+//     }
+// }
+
+// function make_optimized_search(A) {
+//     const xs = copy(A);
+//     selection_sort(xs);
+//     return x => 
+// }
+// qn 2
+
+function fibs(n) {
+    const fibi = [0, 1];
+    for (let i = 2; i <= n; i = i + 1) {
+        fibi[i] = fibi[i - 2] + fibi[i - 1];
+        
     }
-}
-function selection_sort(A) {
-    const len = array_length(A);
-    for (let i = 0; i < len; i = i + 1) {
-        let min_pos = find_min_pos(A, i, len - 1);
-        swap(A, i, min_pos);
-    }
+    return fibi;
 }
 
-function make_optimized_search(A) {
-    const xs = copy(A);
-    selection_sort(xs);
-    return x => 
-}
+fibs(5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
